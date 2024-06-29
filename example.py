1578e6a6e1e8c23e19f81fc473812e34eb2f10f4
Wally Trenholm <wally416@gmail.com>
2024-06-29 05:44:49 -0400

    .

--- example.py
+++ example.py
@@ -1,2 +1,19 @@
+class Address:
+  street: str
+  city: str
+
 class Customer: 
+  first: str
+  last: str
+
+  address: Address
+
+if __name__ == '__main__':
+
+  # create CRDs
+
+  # generate 10 fake objects
+
+  # create obj manifests for those fake objects
+
   pass
