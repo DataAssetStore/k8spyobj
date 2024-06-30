8ec4020229e285f25282da361913fa93d0bbd5cb
Wally Trenholm <wally416@gmail.com>
2024-06-30 07:21:44 -0400

    completed desired example/simple.py

--- examples/simple.py
+++ examples/simple.py
@@ -12,10 +12,20 @@ class Customer:
 
 if __name__ == '__main__':
 
-  # create CRDs
+  # this should load with the default k8s connection configuration
+  with k8spyobj.connect_default_cluster() as kconn:
+    
+    with kconn.new_session() as ksess:
+      for i in [Address,Customer]:
+        # this ensures that the cluster has a CRD for the class, and creates on if not
+        crd = ksess.ensure_crd(i)                                      
+        
+        for index in range(10):                       # generate 10 fake objects for each class
+          o = i()        
 
-  # generate 10 fake objects
+          # create obj manifests for this object
 
-  # create obj manifests for those fake objects
+          # k[crd] should present the objects in the cluster as a dict that is keyed on k8s object type
+          ksess[crd].append(o)                            # add this object to the set of objects in the clusters
 
-  pass
+    ksess.commit()       # commit the underlying transaction
