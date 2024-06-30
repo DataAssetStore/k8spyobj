13f74af9cc049be37053151f7edd655cc80062a3
Wally Trenholm <wally416@gmail.com>
2024-06-30 08:21:36 -0400

    .

--- src/python/k8spyobj/__init__.py
+++ src/python/k8spyobj/__init__.py
@@ -1,2 +1,13 @@
 
 
+class K8sBase:
+
+    k8s_name: str             # maps to the name field in the k8s object
+    k8s_namespace: str             # maps to the name field in the k8s object
+
+class K8sCrdBase:
+    """
+    extend this class for creating a CRD
+
+    """
+    pass
